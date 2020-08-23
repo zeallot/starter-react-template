0f86e40f2c3f98ccd7b43fa6c4a7d29cf41972fa
vkaboskin <vkaboskin@aspirity.com>
2020-08-23 19:54:52 +0700

    init project with eslint

---
+++ src/App/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Main from './components/Main/Main';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Main>Main</Main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/App/components/Main/Main.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Main = ({ children }) => (
+  <main>
+    <div className="container">
+      {children}
+    </div>
+  </main>
+);
+
+Main.propTypes = {
+  children: PropTypes.node,
+};
+
+Main.defaultProps = {
+  children: null,
+};
+
+export default Main;
